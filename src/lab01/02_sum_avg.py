67c6da1fd6a9a1dfd12a2714f8617286b9aae412
2BOCXOD2 <ChristLey@yandex.ru>
2025-09-23 11:55:19 +0300

    02

--- src/lab01/02_sum_avg.py
+++ src/lab01/02_sum_avg.py
@@ -5,11 +5,4 @@ b = b.replace(",", ".")
 a = float(a)
 b = float(b)
 summ = a + b
-sr_arifm = summ / 2
-sr_arifm *= 100
-sr_arifm = int(sr_arifm)
-sr_arifm /= 100
-summ *= 100
-summ = int(summ)
-summ /= 100
-print(f"sum = {summ}; avg = {sr_arifm}")
+print(f"sum = {summ:.2f}; avg = {(summ / 2):.2f}")
