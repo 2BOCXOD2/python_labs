3be68d130187d9b81631f239eab8baaf997a6479
2BOCXOD2 <ChristLey@yandex.ru>
2025-10-05 21:03:42 +0300

    uniq

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -1,3 +1,4 @@
+"""
 def min_max(spisok):
     minn = float("inf")
     maxx = -float("inf")
@@ -11,3 +12,18 @@ spisok_1 = [1, 2, -3, 5.6]
 otvet_1 = min_max(spisok_1)
 
 print(otvet_1)
+"""
+
+def unique_sorted(spisok):
+    unique_spisok = []
+    for i in spisok:
+        if i in unique_spisok:
+            pass
+        else:
+            unique_spisok.append(i)
+    unique_spisok.sort()
+    return unique_spisok
+
+spisok_2 = [3, 3, 4, 2, 1, 1, 2, 5, -1.2, -3.6, -1.2]
+otvet_2 = unique_sorted(spisok_2)
+print(otvet_2)
