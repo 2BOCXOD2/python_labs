a18031a49f0c1dcce876b546aa71a88a03708f84
2BOCXOD2 <ChristLey@yandex.ru>
2025-10-07 00:27:08 +0300

    transpose

---
+++ src/lab02/matrix.py
@@ -0,0 +1,19 @@
+def transpose(matrica):
+    if matrica == []:
+        return []
+    else:
+        stroki = len(matrica[0])
+        stolbci = len(matrica)
+        new_matrica = []
+        for i in matrica:
+            if stroki != len(i):
+                return ValueError
+        for x in range(stroki):
+            spi = []
+            for y in range(stolbci):
+                spi.append(matrica[y][x])
+            new_matrica.append(spi)
+        return new_matrica
+matrica_1 = [[1, 2], [3, 4]]
+otvet_1 = transpose(matrica_1)
+print(otvet_1)     
