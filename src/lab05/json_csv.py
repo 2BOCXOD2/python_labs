1f63f9a63142494a8ef69e34e0422feae1735983
2BOCXOD2 <Christley@yandex.ru>
2025-10-27 21:37:58 +0300

    lab05 0102

--- src/lab05/json_csv.py
+++ src/lab05/json_csv.py
@@ -2,31 +2,54 @@ def json_to_csv(json_path: str, csv_path: str):
     from pathlib import Path
     import json
     import csv
-    if Path(json_path).exists():
+    if Path(json_path).exists(): # Проверка существования пути
         if json_path[-5:] == ".json" and csv_path[-4:] == ".csv":
             f = open(json_path, "r", encoding="utf-8")
             st = f.read()
             f.close()
             if len(st) == 0:
-                raise ValueError
+                raise ValueError # Ошибка, если файл пустой
             else:
                 with open(json_path, "r", encoding="utf-8") as fj:
                     data = json.load(fj)
                 x = data[0].keys() # Сохраняем изначальный порядок колонок
                 with open(csv_path, "w", newline='', encoding="utf-8") as fc:
-                    writer = csv.DictWriter(fc, fieldnames=x, extrasaction="raise")
+                    writer = csv.DictWriter(fc, fieldnames=x, extrasaction="raise") # Переменная для удобной записи (файл для записи, порядок колонок, вывод ошибки если ключ не найден)
                     writer.writeheader()
                     writer.writerows(data)
-
-
         else:
-            raise ValueError
+            raise ValueError # Ошибка, если указан не верный тип файла
     else:
-        raise FileNotFoundError
-    
-a = json_to_csv("././data/samples/people.json", "././data/out/people.csv")
+        raise FileNotFoundError # Ошибка, если файл не найден
+# a = json_to_csv("././data/samples/people.json", "././data/out/people.csv")
 
 # data = [{"name": "Alice", "age": 22}, {"name": "Bob", "age": 25}]
 # path = Path("././data/out/people.json")
 # with open(path, "w", encoding="utf-8") as fj:
 #     json.dump(data, fj,  ensure_ascii=False, indent=2)
+
+
+def csv_to_json(csv_path: str, json_path: str):
+    from pathlib import Path
+    import json
+    import csv
+    if Path(csv_path).exists(): # Проверка существования пути
+        if json_path[-5:] == ".json" and csv_path[-4:] == ".csv": # Проверка формата файлов
+            f = open(csv_path, "r", encoding="utf-8")
+            st = f.read()
+            f.close()
+            if len(st) == 0:
+                raise ValueError # Ошибка, если файл пустой
+            else:
+                with open(csv_path, "r", encoding="utf-8") as fc: # Открываем файл csv в режиме чтения
+                    reader = csv.DictReader(fc) # Записываем чтение в переменную
+                    data = [] # Создаём переменную для хранения словарей
+                    for row in reader: # Для каждого словаря
+                        data.append(row) # записываем его в список
+                with open(json_path, "w", newline='', encoding="utf-8") as fj: # Открываем json файл
+                    json.dump(data, fj, ensure_ascii=False, indent=2) # Записываем в него список словарей, выводя только ASCII символы, отступ уровня важности равен 2
+        else:
+            raise ValueError # Ошибка, если указан не верный тип файла
+    else:
+        raise FileNotFoundError # Ошибка, если файл не найден
+a = csv_to_json("././data/samples/people1.csv", "././data/out/people1.json")
